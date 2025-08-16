1680435f03da122b6897601a5a84e3a848b92ba9
david <david@example.com>
2025-08-16 15:05:49 -0700

    Remove test formatting file

--- app/src/main/java/com/wearinterval/TestFormat.kt
+++
@@ -1,9 +0,0 @@
-// Test file to check formatting hook
-package com.wearinterval
-
-class TestFormat {
-    val badlyFormatted = true
-    fun test() {
-        println("badly formatted again")
-    }
-}
