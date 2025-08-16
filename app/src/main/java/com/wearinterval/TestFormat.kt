8219628486b153151eac37267db1d4785a90a961
david <david@example.com>
2025-08-16 15:16:03 -0700

    Remove test file

--- app/src/main/java/com/wearinterval/TestFormat.kt
+++
@@ -1,9 +0,0 @@
-// Test auto-format hook
-package com.wearinterval
-
-class TestAutoFormat {
-    val badSpacing = true
-    fun poorlyFormatted()  {
-        println("this will be auto-formatted")
-    }
-}
